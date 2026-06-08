970d58ec10e72253559a3161a4455c9987946158
Geoff Vargo <geoffvargo@gmail.com>
2026-06-08 18:09:37 -0400

    created backend skeleton.

---
+++ backend/gvoRBA Backend/src/main/java/com/geoffvargo/gvorbabackend/GvoRbaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.geoffvargo.gvorbabackend;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+
+@SpringBootApplication
+public class GvoRbaBackendApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(GvoRbaBackendApplication.class, args);
+	}
+	
+}
---
+++ backend/gvoRBA Backend/src/test/java/com/geoffvargo/gvorbabackend/GvoRbaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geoffvargo.gvorbabackend;
+
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.context.*;
+
+@SpringBootTest
+class GvoRbaBackendApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
