1e810c46d387644a551c3e8bdd015f6baaebab32
Geoff Vargo <geoffvargo@gmail.com>
2026-07-26 02:40:09 -0400

    finished fixing `navbar`

--- frontend/gvorba-frontend/src/app/navbar/navbar.component.ts
+++ frontend/gvorba-frontend/src/app/navbar/navbar.component.ts
@@ -1,8 +1,6 @@
-import { Component, inject, signal, OnInit, OnDestroy } from '@angular/core';
-import { ApiService } from '../services/api.service';
-import { JwtHelperService } from '@auth0/angular-jwt';
+import { Component, inject, signal } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { AuthStore } from '../stores/auth-store';
 
 @Component({
 	selector: 'app-navbar',
@@ -12,59 +10,19 @@ import { Subscription } from 'rxjs';
 	templateUrl: './navbar.component.html',
 	styleUrl: './navbar.component.css',
 })
-export class NavbarComponent implements OnInit, OnDestroy {
-	private subscription!: Subscription;
-	private apiService = inject(ApiService);
-	private jwtHelper = inject(JwtHelperService);
-	
+export class NavbarComponent {
+	private authStore = inject(AuthStore);
 	protected router = inject(Router);
 	
-	isAdmin = signal(false);
-	isLoggedIn = signal(false);
+	protected readonly isLoggedIn = this.authStore.isAuthenticated;
+	protected readonly isAdmin = this.authStore.isAdmin;
+	protected readonly isMember = this.authStore.isMember;
+	protected readonly role = this.authStore.role;
 	
-	authToken = signal(sessionStorage.getItem('auth-token'));
-	menuVisible = signal(false);
-	role = signal('');
-	
-	ngOnInit() {
-		this.subscription = this.apiService.isloggedIn$.subscribe(() => {
-			this.refreshAuthState();
-		});
-		
-		this.refreshAuthState();
-	}
-	
-	ngOnDestroy() {
-		this.subscription?.unsubscribe();
-	}
+	protected menuVisible = signal(false);
 	
 	onLogout() {
-		this.apiService.logout();
+		this.authStore.logout();
 		this.router.navigateByUrl('').then();
-	}
-	
-	private refreshAuthState() {
-		const token = sessionStorage.getItem('auth-token');
-		this.authToken.set(token);
-		
-		if (!token || this.jwtHelper.isTokenExpired(token)) {
-			this.isLoggedIn.set(false);
-			this.isAdmin.set(false);
-			this.role.set('');
-			
-			return;
-		}
-		
-		const payload = this.decodeJwt(token);
-		this.role.set(payload.roles);
-		
-		this.isLoggedIn.set(true);
-		this.isAdmin.set(payload.roles === 'ROLE_ADMIN');
-	}
-	
-	private decodeJwt(token: string): { roles: string } {
-		const payload = token.split('.')[1];
-		const decodedPayload = atob(payload);
-		return JSON.parse(decodedPayload);
 	}
 }
