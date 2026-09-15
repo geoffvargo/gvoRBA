536f304a418fced98dec86b0483d1eba3b1d9ccb
Geoff Vargo <geoffvargo@gmail.com>
2026-09-15 13:41:15 -0400

    made the styling for navbar.component responsive

--- frontend/gvorba-frontend/src/app/navbar/navbar.component.ts
+++ frontend/gvorba-frontend/src/app/navbar/navbar.component.ts
@@ -1,11 +1,13 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { AuthStore } from '../stores/auth-store';
+import { NgClass } from '@angular/common';
 
 @Component({
 	selector: 'app-navbar',
 	imports: [
 		RouterLink,
+		NgClass,
 	],
 	templateUrl: './navbar.component.html',
 	styleUrl: './navbar.component.css',
@@ -18,8 +20,15 @@ export class NavbarComponent {
 	protected readonly isAdmin = this.authStore.isAdmin;
 	protected readonly role = this.authStore.role;
 	
+	menuVisible = signal(false);
+	
 	onLogout() {
+		this.menuVisible.set(false);
 		this.authStore.logout();
 		this.router.navigateByUrl('').then();
+	}
+	
+	toggleMenu() {
+		this.menuVisible.update(state => !state);
 	}
 }
